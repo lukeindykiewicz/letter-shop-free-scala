79908c63d2ca71c9dbcb7c9785cdfdeec6a0bdaf
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-21 17:50:42 +0200

    Add get and put cart paths

---
+++ src/main/scala/main.scala
@@ -0,0 +1,64 @@
+package lettershop.free
+
+import akka.actor._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.stream.ActorMaterializer
+
+object Main
+    extends App
+    with Routes
+    with Programs
+    with Compilers {
+
+  implicit val system = ActorSystem("letter-shop-free")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+}
+
+
+trait Routes {
+  self: Programs with Compilers =>
+
+  lazy val route =
+    pathPrefix("cart") {
+      getCart ~ putCart //~ postCart
+    }
+  // ~
+  //     putPrice ~
+  //     checkCart ~
+  //     checkoutCart ~
+  //     getReceipts
+
+  import spray.json.DefaultJsonProtocol._
+  implicit val cartJsonFormat = CartJsonSupport.CartFormats
+  implicit val priceJsonFormat = PriceJsonSupport.PriceFormats
+  implicit val checkoutJsonFormat = CheckoutJsonSupport.CheckoutFormats
+  implicit val receiptHistoryFormat = ReceiptHistoryJsonSupport.ReceiptHistoryFormats
+
+  val storageCmp = storageCompiler
+
+  lazy val getCart =
+    get {
+      path(Segment) { cartId =>
+        complete( getCartProgram(cartId).foldMap(storageCmp)  )
+      }
+    }
+
+    lazy val putCart =
+    put {
+      path(Segment / Segment) { (cartId, letters) =>
+        addToCartProgram(cartId, letters).foldMap(storageCmp)
+        complete(OK)
+      } ~
+      path(Segment) { cartId =>
+        addToCartProgram(cartId, "").foldMap(storageCmp)
+        complete(OK)
+      }
+    }
+
+}
