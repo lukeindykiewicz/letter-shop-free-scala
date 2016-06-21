6aeb30c3c108a361c3a00fcdf4d8b689dff66bad
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-21 17:51:25 +0200

    Add programs and compiler for get and put ops

---
+++ src/main/scala/programs.scala
@@ -0,0 +1,42 @@
+package lettershop.free
+
+trait Programs
+    extends StorageConstructors {
+
+  def getCartProgram(cartId: String): Storage[Cart] =
+    for {
+     cart <- getCart(cartId) 
+    } yield cart
+
+  def addToCartProgram(cartId: String, letters: String): Storage[Unit] =
+    for {
+      _ <- addToCart(cartId, letters)
+    } yield ()
+
+}
+
+trait Compilers {
+
+  import cats.{Id, ~>}
+
+  def storageCompiler: StorageADT ~> Id =
+    new (StorageADT ~> Id) {
+
+      import scala.collection.concurrent.TrieMap
+      var carts: TrieMap[String, String] = TrieMap.empty
+      var prices: TrieMap[String, Double] = TrieMap.empty
+      var receipts: TrieMap[String, ReceiptHistory] = TrieMap.empty
+
+      def apply[A](fa: StorageADT[A]): Id[A] = fa match {
+        case GetCart(cartId) => Cart(carts.getOrElse(cartId, ""))
+        case AddToCart(cartId, letters) =>
+          carts += (cartId -> letters)
+          ()
+        case _ =>
+          println("Not implemented yet")
+          "not implemented".asInstanceOf[A]
+      }
+
+    }
+
+}
