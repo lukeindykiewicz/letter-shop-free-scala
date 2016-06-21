74e32d4b9bc3405620a66772f2b338d0849db131
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-21 17:49:35 +0200

    Describe domain ops as ADTs

--- src/main/scala/domain.scala
+++ src/main/scala/domain.scala
@@ -1,15 +1,43 @@
 package lettershop.free
 
-case class Cart(letters: String)
-case class Price(price: Double)
-case class Checkout(price: Double, receiptId: String)
-case class ReceiptHistory(price: Double, receiptId: String, letters: String)
+  case class Cart(letters: String)
+  case class Price(price: Double)
+  case class Checkout(price: Double, receiptId: String)
+  case class ReceiptHistory(price: Double, receiptId: String, letters: String)
 
-sealed trait LetterShopADT[A]
-case class GetCart(cartId: String) extends LetterShopADT[Cart]
-case class UpdateCart(cartId: String, letters: String) extends LetterShopADT[Unit]
-case class OverrideCart(cartdId: String, letters: String) extends LetterShopADT[Unit]
-case class CheckCart(cartId: String, discountCode: Option[String]) extends LetterShopADT[Price]
-case class CheckoutCart(cartId: String, discountCode: Option[String]) extends LetterShopADT[Checkout]
-case class PriceTheLetter(letter: String, price: Double) extends LetterShopADT[Unit]
-case object GetReceipts extends LetterShopADT[ReceiptHistory]
+
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+  import spray.json._
+
+  object CartJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val CartFormats = jsonFormat1(Cart)
+  }
+
+  object PriceJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val PriceFormats = jsonFormat1(Price)
+  }
+
+  object CheckoutJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val CheckoutFormats = jsonFormat2(Checkout)
+  }
+
+  object ReceiptHistoryJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val ReceiptHistoryFormats = jsonFormat3(ReceiptHistory)
+  }
+
+  sealed trait StorageADT[A]
+  case class AddToCart(cartId: String, letters: String) extends StorageADT[Unit]
+  case class UpdateCart(cartId: String, letters: String) extends StorageADT[Unit]
+  case class GetCart(cartId: String) extends StorageADT[Cart]
+  case class AddToPrices(letter: String, price: Double) extends StorageADT[Unit]
+  case object GetPrices extends StorageADT[Map[String, Price]]
+  case object GetReceipts extends StorageADT[ReceiptHistory]
+
+
+  sealed trait PromoADT[A]
+  case class CountPromotion(letters: String, promoCode: String) extends PromoADT[Double]
+
+
+// sealed trait ValidADT[A]
+// IsLetter
+// IsMoreThanZero
--- src/main/scala/free.scala
+++ src/main/scala/free.scala
@@ -2,27 +2,33 @@ package lettershop
 
 package object free {
   import cats.free.Free
-  type LetterShop[A] = Free[LetterShopADT, A]
+
+  type Storage[A] = Free[StorageADT, A]
+  type Promotion[A] = Free[PromoADT, A]
 }
 
 package free {
 
   import cats.free.Free.liftF
 
-  trait LetterShopConstructors {
-    def getCart(cartId: String): LetterShop[Cart] =
-      liftF[LetterShopADT, Cart](GetCart(cartId))
-    def updateCart(cartId: String, letters: String): LetterShop[Unit] =
-      liftF[LetterShopADT, Unit](UpdateCart(cartId, letters: String))
-    def overrideCart(cartId: String, letters: String): LetterShop[Unit] =
-      liftF[LetterShopADT, Unit](OverrideCart(cartId, letters: String))
-    def checkCart(cartId: String, discountCode: Option[String]): LetterShop[Price] =
-      liftF[LetterShopADT, Price](CheckCart(cartId, discountCode))
-    def checkoutCart(cartId: String, discountCode: Option[String]): LetterShop[Checkout] =
-      liftF[LetterShopADT, Checkout](CheckoutCart(cartId, discountCode))
-    def priceTheLetter(letter: String, price: Double): LetterShop[Unit] =
-      liftF[LetterShopADT, Unit](PriceTheLetter(letter, price))
-    def getReceipts: LetterShop[ReceiptHistory] =
-      liftF[LetterShopADT, ReceiptHistory](GetReceipts)
+  trait StorageConstructors {
+    def addToCart(cartId: String, letters: String): Storage[Unit] =
+      liftF[StorageADT, Unit](AddToCart(cartId, letters))
+    def updateCart(cartId: String, letters: String): Storage[Unit] =
+      liftF[StorageADT, Unit](UpdateCart(cartId, letters))
+    def getCart(cartId: String): Storage[Cart] =
+      liftF[StorageADT, Cart](GetCart(cartId))
+    def addToPrices(letter: String, price: Double): Storage[Unit] =
+      liftF[StorageADT, Unit](AddToPrices(letter, price))
+    def getPrices: Storage[Map[String, Price]] =
+      liftF[StorageADT, Map[String, Price]](GetPrices)
+    def getReceipts: Storage[ReceiptHistory] =
+      liftF[StorageADT, ReceiptHistory](GetReceipts)
   }
+
+  trait PromoConstructors {
+    def countPromotion(letters: String, promoCode: String): Promotion[Double] =
+      liftF[PromoADT, Double](CountPromotion(letters, promoCode))
+  }
+
 }
